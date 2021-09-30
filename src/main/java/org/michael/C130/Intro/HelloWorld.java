dd1354da3f5f953632d6f4fa8457ca0729d496f1
Michael U <Me@michael.com>
2021-09-30 15:02:45 +0100

    002: HelloWorld.java

--- src/main/java/org/michael/C130/Intro/HelloWorld.java
+++ src/main/java/org/michael/C130/Intro/HelloWorld.java
@@ -3,6 +3,6 @@ package org.michael.C130.Intro;
 public class HelloWorld {
 
     public static void main( String[] args){
-        System.out.print("Hello World");
+        System.out.print("Hello World ");
     }
 }
