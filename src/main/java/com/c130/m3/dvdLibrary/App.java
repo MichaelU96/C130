2d4d9a2ae7638432f06af0a5c496518061f6553b
Michael U <Me@michael.com>
2021-10-14 14:36:42 +0100

    App.Java

--- src/main/java/com/c130/m3/dvdLibrary/App.java
+++ src/main/java/com/c130/m3/dvdLibrary/App.java
@@ -1,13 +1,67 @@
 package com.c130.m3.dvdLibrary;
 
 import com.c130.m3.dvdLibrary.Dao.DVDLibDao;
+import com.c130.m3.dvdLibrary.Dao.DVDLibraryDaoFilemp;
+import com.c130.m3.dvdLibrary.UI.DVDLibraryView;
+import com.c130.m3.dvdLibrary.UI.UserIO;
+import com.c130.m3.dvdLibrary.controller.DVDLibraryController;
 
 public class App {
         public static void main(String[] args) {
-            UserIO io = new UserIOImpl();
-            DVDLibView view = new DVDLibView(io);
-            DVDLibDao dao = new DVDDaoFileImpl();
-            DVDLibController controller = new DVDLibController(view, dao);
+            UserIO io = new UserIO() {
+                @Override
+                public void print(String msg) {
+
+                }
+
+                @Override
+                public double readDouble(String prompt) {
+                    return 0;
+                }
+
+                @Override
+                public double readDouble(String prompt, double min, double max) {
+                    return 0;
+                }
+
+                @Override
+                public float readFloat(String prompt) {
+                    return 0;
+                }
+
+                @Override
+                public float readFloat(String prompt, float min, float max) {
+                    return 0;
+                }
+
+                @Override
+                public int readInt(String prompt) {
+                    return 0;
+                }
+
+                @Override
+                public int readInt(String prompt, int min, int max) {
+                    return 0;
+                }
+
+                @Override
+                public long readLong(String prompt) {
+                    return 0;
+                }
+
+                @Override
+                public long readLong(String prompt, long min, long max) {
+                    return 0;
+                }
+
+                @Override
+                public String readString(String prompt) {
+                    return null;
+                }
+            };
+            DVDLibraryView view = new DVDLibraryView(io);
+            DVDLibDao dao = new DVDLibraryDaoFilemp();
+            DVDLibraryController controller = new DVDLibraryController(view, dao);
             controller.run();
         }
 }
